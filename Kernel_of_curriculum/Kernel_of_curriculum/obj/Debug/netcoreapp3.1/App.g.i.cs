9822cb99162096ca9e25a33e8ba1c86cecb979ab
Vadim Belous <belous.vadim@inbox.ru>
2022-02-20 22:02:23 +0300

    20_02

--- Kernel_of_curriculum/Kernel_of_curriculum/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ Kernel_of_curriculum/Kernel_of_curriculum/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Kernel_of_curriculum {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Kernel_of_curriculum {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public static void Main() {
             Kernel_of_curriculum.App app = new Kernel_of_curriculum.App();
             app.InitializeComponent();
