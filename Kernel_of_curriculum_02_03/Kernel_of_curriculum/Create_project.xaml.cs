90ffb41e66758172448d30d80b24a18f03698729
Vadim Belous <belous.vadim@inbox.ru>
2022-03-02 22:47:52 +0300

    02_03

---
+++ Kernel_of_curriculum_02_03/Kernel_of_curriculum/Create_project.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Kernel_of_curriculum {
+    /// <summary>
+    /// Логика взаимодействия для Create_project.xaml
+    /// </summary>
+    public partial class Create_project : Window
+    {
+        public Create_project()
+        {
+            InitializeComponent();
+        }
+    }
+}
